ca410a0d456afafb3763f760f738b27d285b0a5f
Lupux <ekblom.j.p@gmail.com>
2016-07-11 08:44:20 +0200

    Created Main_dev for development

---
+++ KAB9/KAB9/Master_Kund.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace KAB9
+{
+    public class Master_Kund : Master_Admin
+    {
+        public void viewOrderHistory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MyPages()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ChangePassword()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MakeBuy()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ KAB9/KAB9/Master_admin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace KAB9
+{
+    public class Master_Admin
+    {
+        public void ProductHandeling()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CustomerHandeling()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OrderHistoryHandeling()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
