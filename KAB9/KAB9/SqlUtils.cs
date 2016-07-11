c0e864057f89e30029d3df1c7ab2a78cfb6f819f
Timothy Boris Jalgard <timothy.jalgard@gmail.com>
2016-07-11 16:52:49 +0200

    Har börjat jobba i SQLUtils. Skapat LoadProducts.

--- KAB9/KAB9/SqlUtils.cs
+++ KAB9/KAB9/SqlUtils.cs
@@ -8,5 +8,40 @@ namespace KAB9
 {
     public class SqlUtils
     {
+        static string conStr = "";
+
+        static SqlConnection dBConnection = new SqlConnection(conStr);
+    }
+    public static List<Product> LoadProducts()
+    {
+        List<Product> tmpList = new List<Contact>();
+        SqlCommand dBCommand = new SqlCommand("select " +
+                                              "* from Product and " +
+                                              "Stock " +
+                                              "order by ID", dBConnection);
+        try
+        {
+            dBConnection.Open();
+            SqlDataReader dBReader = dBCommand.ExecuteReader();
+            while (Reader.Read())
+            {
+                int productid = Convert.ToInt32(dBReader["ProductID"]);
+                string productName = dBReader["ProductName"].ToString();
+                string description = dBReader["Description"].ToString();
+                double price= Convert.ToDouble(dBReader["Price"]);
+                int stock = Convert.ToInt32(dBReader["NumberofItems"]);
+
+                tmpList.Add(new Product(Productid, productName, description, price, stock));
+            }
+        }
+        catch (Exception)
+        {
+            //Response.Write($"<script>alert('{ex.Message}');</script>");
+        }
+        finally
+        {
+            myConnection.Close();
+        }
+        return tmpList;
     }
 }
