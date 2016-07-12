71f17f8fef08bc894dabf45958c0ba25471e5ad9
Timothy Boris Jalgard <timothy.jalgard@gmail.com>
2016-07-12 10:36:19 +0200

    SQLUtils har nu full CRUD på Customer och Product

--- KAB9/KAB9/SqlUtils.cs
+++ KAB9/KAB9/SqlUtils.cs
@@ -17,10 +17,13 @@ namespace KAB9
         public static List<Product> LoadProducts()
         {
             List<Product> tmpList = new List<Product>();
-            SqlCommand dBCommand = new SqlCommand("select " +
-                                                  "* from Product and " +
-                                                  "Stock " +
-                                                  "order by ProductId", dBConnection);
+
+            //SqlCommand dBCommand = new SqlCommand("select " +
+            //                                      "* from Product and " +
+            //                                      "Stock " +
+            //                                      "order by ProductId", dBConnection);
+            SqlCommand dBCommand = new SqlCommand("spAddProduct", dBConnection);
+            dBCommand.CommandType = CommandType.StoredProcedure;
             try
             {
                 dBConnection.Open();
@@ -73,9 +76,9 @@ namespace KAB9
                 paramAddToStock.Value = addToStock;
                 dBCommand.Parameters.Add(paramAddToStock);
 
-                SqlParameter paramProductID = new SqlParameter("@productID", SqlDbType.Int);
-                paramProductID.Direction = ParameterDirection.Output;
-                dBCommand.Parameters.Add(paramProductID);
+                SqlParameter paramProductid = new SqlParameter("@productid", SqlDbType.Int);
+                paramProductid.Direction = ParameterDirection.Output;
+                dBCommand.Parameters.Add(paramProductid);
 
                 int result = dBCommand.ExecuteNonQuery();
 
@@ -95,6 +98,88 @@ namespace KAB9
             }
         }
 
+        public static void UpdateProduct(string productName, string description, double price, int addToStock, int productID)
+        {
+            if (productID != null)
+            {
+                try
+                {
+                    SqlCommand dBCommand = new SqlCommand("spUpdateProduct", dBConnection);
+                    dBCommand.CommandType = CommandType.StoredProcedure;
+                    dBConnection.Open();
+
+                    SqlParameter paramProductID = new SqlParameter("@productID", SqlDbType.Int);
+                    paramProductID.Value = productName;
+                    dBCommand.Parameters.Add(paramProductID);
+
+                    if (productName != "")
+                    {
+                        SqlParameter paramProductName = new SqlParameter("@productName", SqlDbType.VarChar);
+                        paramProductName.Value = productName;
+                        dBCommand.Parameters.Add(paramProductName);
+                    }
+
+                    if (description != "")
+                    {
+                        SqlParameter paramDescription = new SqlParameter("@description", SqlDbType.VarChar);
+                        paramDescription.Value = description;
+                        dBCommand.Parameters.Add(paramDescription);
+                    }
+
+                    if (price != null)
+                    {
+
+                        try
+                        {
+                            SqlParameter paramPrice = new SqlParameter("@price", SqlDbType.Money);
+                            paramPrice.Value = price;
+                            dBCommand.Parameters.Add(paramPrice);
+                        }
+                        catch (Exception)
+                        {
+                            /*alert*/
+                            /*priceTextBox = ("Du angav ett ogiltigt pris. Försök igen");*/
+                        }
+                    }
+
+                    if (addToStock != null)
+                    {
+                        try
+                        {
+                            SqlParameter paramAddToStock = new SqlParameter("@addToStock", SqlDbType.Int);
+                            paramAddToStock.Value = addToStock;
+                            dBCommand.Parameters.Add(paramAddToStock);
+
+                        }
+                        catch (Exception)
+                        {
+                            /*alert*/
+                            /*addToStockTextBox = ("Du angav ett ogiltigt antal för lagerhållning. Försök igen");*/
+                        }
+                    }
+
+                    int result = dBCommand.ExecuteNonQuery();
+
+                    if (result == 0)
+                    {
+                        /*alert*/
+                        /*firstName = ("ERROR when adding contact to Database!");*/
+                    }
+                }
+                catch (Exception)
+                {
+                    /*lastName = "FEL vid laddning av kontakter från SQL!";*/
+                }
+                finally
+                {
+                    dBConnection.Close();
+                }
+            }
+            else
+            { /*alert*/
+              /*VISA EN ALERT!: "Dett har skett en miss vid valet av produkt att uppdatera"*/
+            }
+        }
         public static void DeleteProduct(int productid)
         {
             try
@@ -120,5 +205,184 @@ namespace KAB9
                 dBConnection.Close();
             }
         }
+
+        public static List<Customer> LoadCustomers()
+        {
+            List<Customer> tmpList = new List<Customer>();
+
+            //SqlCommand dBCommand = new SqlCommand("select " +
+            //                                      "* from Customer "+
+            //                                      "order by CustomerId", dBConnection);
+
+
+            SqlCommand dBCommand = new SqlCommand("spAddCustomer", dBConnection);
+            dBCommand.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                dBConnection.Open();
+                SqlDataReader dBReader = dBCommand.ExecuteReader();
+                while (dBReader.Read())
+                {
+                    int customerID = Convert.ToInt32(dBReader["CustomerID"]);
+                    string firstName = dBReader["FirstName"].ToString();
+                    string lastName = dBReader["LastName"].ToString();
+                    string email = dBReader["EmailAddress"].ToString();
+                    string phoneNr = dBReader["PhoneNr"].ToString();
+
+                    tmpList.Add(new Customer(customerID, firstName, lastName, email, phoneNr));
+                }
+            }
+            catch (Exception ex)
+            {
+                //<script>alert('{ex.Message}');</script>;
+            }
+            finally
+            {
+                dBConnection.Close();
+            }
+            return tmpList;
+        }
+
+        public static void AddCustomer(string firstName, string lastName, string emailAddress, string phoneNr)
+        {
+            try
+            {
+                SqlCommand dBCommand = new SqlCommand();
+                dBCommand.CommandType = CommandType.StoredProcedure;
+                dBCommand.CommandText = "spAddCustomer";
+                dBCommand.Connection = dBConnection;
+                dBConnection.Open();
+
+                SqlParameter paramFirstName = new SqlParameter("@FirstName", SqlDbType.VarChar);
+                paramFirstName.Value = firstName;
+                dBCommand.Parameters.Add(paramFirstName);
+
+                SqlParameter paramLastName = new SqlParameter("@LastName", SqlDbType.VarChar);
+                paramLastName.Value = lastName;
+                dBCommand.Parameters.Add(paramLastName);
+
+                SqlParameter paramEmailAddress = new SqlParameter("@EmailAddress", SqlDbType.VarChar);
+                paramEmailAddress.Value = emailAddress;
+                dBCommand.Parameters.Add(paramEmailAddress);
+
+                SqlParameter paramPhoneNr = new SqlParameter("@phoneNr", SqlDbType.VarChar);
+                paramPhoneNr.Value = phoneNr;
+                dBCommand.Parameters.Add(paramPhoneNr);
+
+                SqlParameter paramCustomerid = new SqlParameter("@customerid", SqlDbType.Int);
+                paramCustomerid.Direction = ParameterDirection.Output;
+                dBCommand.Parameters.Add(paramCustomerid);
+
+                int result = dBCommand.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    /*alert*/
+                    /*firstName = ("ERROR when adding Customer to Database!");*/
+                }
+            }
+            catch (Exception)
+            {
+                /*lastName = "FEL vid tilläg av kund till Databas!";*/
+            }
+            finally
+            {
+                dBConnection.Close();
+            }
+        }
+
+        public static void UpdateCustomer(string firstName, string lastName, string emailAddress, string phoneNr, int customerid)
+        {
+            if (customerid != null)
+            {
+                try
+                {
+                    SqlCommand dBCommand = new SqlCommand("spUpdateCustomer", dBConnection);
+                    dBCommand.CommandType = CommandType.StoredProcedure;
+                    dBConnection.Open();
+
+                    SqlParameter paramCustomerid = new SqlParameter("@customerid", SqlDbType.Int);
+                    paramCustomerid.Value = customerid;
+                    dBCommand.Parameters.Add(paramCustomerid);
+
+                    if (firstName != "")
+                    {
+                        SqlParameter paramFirstName = new SqlParameter("@FirstName", SqlDbType.VarChar);
+                        paramFirstName.Value = firstName;
+                        dBCommand.Parameters.Add(paramFirstName);
+                    }
+
+                    if (lastName != "")
+                    {
+                        SqlParameter paramLastName = new SqlParameter("@LastName", SqlDbType.VarChar);
+                        paramLastName.Value = lastName;
+                        dBCommand.Parameters.Add(paramLastName);
+                    }
+
+                    if (emailAddress != "")
+                    {
+                        SqlParameter paramEmailAddress = new SqlParameter("@EmailAddress", SqlDbType.VarChar);
+                        paramEmailAddress.Value = emailAddress;
+                        dBCommand.Parameters.Add(paramEmailAddress);
+                    }
+
+
+                    if (phoneNr != "")
+                    { 
+                            SqlParameter paramPhoneNr = new SqlParameter("@phoneNr", SqlDbType.VarChar);
+                            paramPhoneNr.Value = phoneNr;
+                            dBCommand.Parameters.Add(paramPhoneNr);
+                    }
+
+                    int result = dBCommand.ExecuteNonQuery();
+
+                    if (result == 0)
+                    {
+                        /*alert*/
+                        /*firstName = ("ERROR when adding Customer to Database!");*/
+                    }
+                }
+                catch (Exception)
+                {
+                    /*lastName = "FEL vid uppdatering av kund till Databas!";*/
+                }
+                finally
+                {
+                    dBConnection.Close();
+                }
+            }
+            else
+            { /*alert*/
+              /*VISA EN ALERT!: "Det har skett en miss vid valet av Kund att uppdatera"*/
+            }
+        }
+        public static void DeleteCustomer (int customerid)
+        {
+            try
+            {
+                SqlCommand dBCommand = new SqlCommand();
+
+                dBCommand.CommandText = $"delete Customer where CustomerID = '{customerid}'";
+                dBCommand.Connection = dBConnection;
+                dBConnection.Open();
+                int result = dBCommand.ExecuteNonQuery();
+                if (result == 0)
+                {
+                    // MÖÖÖÖG
+                }
+            }
+            catch (Exception)
+            {
+                //Response.Write($"<script>alert('{ex.Message}');</script>");
+            }
+
+            finally
+            {
+                dBConnection.Close();
+            }
+        }
     }
+
+
+
 }
